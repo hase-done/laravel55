ae0389402b90b008574472281119f5389661eb8a
hase_done <hasegawa1326@gmail.com>
2019-01-01 05:17:51 +0900

    ルーティング

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,54 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('hello', function () {
+    return '<html><body>hello</body></html>';
+});
+
+
+
+Route::get('hello2', function () {
+    return 'hello2';
+});
+
+
+
+$html = <<<EOF
+<html>
+<body>
+hello3
+</body>
+</html>
+EOF;
+Route::get('hello3', function () use ($html) {
+    return $html;
+});
+
+
+
+Route::get('hello4/{msg}/{id}', function ($msg, $id) {
+  $html = <<<EOF
+  <html>
+  <body>
+  hello4 {$msg} {$id}
+  </body>
+  </html>
+EOF;
+    return $html;
+});
+
+
+// ?つければ無くても大丈夫なパターン この場合だと0になる
+Route::get('hello5/{msg}/{id?}', function ($msg, $id=0) {
+  $html = <<<EOF
+  <html>
+  <body>
+  hello4 {$msg} {$id}
+  </body>
+  </html>
+EOF;
+    return $html;
+});
