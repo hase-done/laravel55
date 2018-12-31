b525cb0b95c04eb2621173f40462da5619f8e2b1
hase_done <hasegawa1326@gmail.com>
2019-01-01 08:07:39 +0900

    【重要】MySQLのインデックスサイズ問題の為、使用インデックス最大長を191バイトにした。

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // mysqlのインデックスサイズに767byteまでしかつかえない問題
+        // https://blog.e2info.co.jp/2017/04/17/mysql%E3%81%AE%E3%82%A4%E3%83%B3%E3%83%87%E3%83%83%E3%82%AF%E3%82%B9%E3%82%B5%E3%82%A4%E3%82%BA%E3%81%AB767byte%E3%81%BE%E3%81%A7%E3%81%97%E3%81%8B%E3%81%A4%E3%81%8B%E3%81%88%E3%81%AA%E3%81%84/
+        Schema::defaultStringLength(191);
     }
 
     /**
